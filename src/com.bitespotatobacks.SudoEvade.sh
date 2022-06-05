720be7d42b45b28a1c8731aa63f6a50a368aba52
BitesPotatoBacks <83843298+BitesPotatoBacks@users.noreply.github.com>
2022-06-05 07:50:38 -0700

    Add files via upload

---
+++ src/com.bitespotatobacks.SudoEvade.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+#  com.bitespotatobacks.SudoEvade.sh
+#  SudoEvade (Helper Tool)
+#
+#  Created by BitesPotatoBacks on 5/31/22.
+#  Copyright (c) 2022 BitesPotatoBacks. All rights reserved.
+#
+
+# Issues:
+# - Shell scripts are broken
+# - some builtin bash scripts complain (such as read)
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+BOLD=$(tput bold)
+DLOB=$(tput sgr0)
+
+DIRSTUB="/Library/Caches/com.bitespotatobacks.SudoEvade"
+CMDFILE="$DIRSTUB/com.bitespotatobacks.SudoEvade.cmd.txt"
+
+NEWCMD=""
+OLDCMD=$(cat $CMDFILE)
+TIME=$(date +"%T")
+
+CMDPATH=""
+
+function stop() {
+    TIME=$(date +"%T")
+    echo "${BOLD}$TIME${DLOB}: ${RED}Qutting${NC}"
+    exit
+}
+
+trap 'stop' SIGINT
+echo "${BOLD}$TIME${DLOB}: Starting SudoEvade ($$)"
+
+while true; do
+    TIME=$(date +"%T")
+    NEWCMD=$(cat $CMDFILE)
+    
+    echo ~
+    echo "${BOLD}$TIME${DLOB}: ${BLUE}Watching ==>${NC} $CMDFILE"
+    
+    # watching our txt file (which contains our command string) for changes
+    # when a change is detected, clone the bianry belonging to the command and modify it's permissions
+    # this is done infinitely until the script is killed
+    
+    if [[ $NEWCMD != $OLDCMD ]] && [[ $NEWCMD != "" ]]; then
+        CMDPATH=$(type -P $(echo "$NEWCMD" | awk '{print $1}'))
+        OLDCMD=$NEWCMD
+        
+        echo "${BOLD}$TIME${DLOB}: ${GREEN}Recieved ==>${NC} $NEWCMD (via client)"
+        
+        cp $CMDPATH $DIRSTUB &
+        echo "${BOLD}$TIME${DLOB}: ${YELLOW}Performing ==>${NC} Cloning binary to $DIRSTUB"
+
+        sudo chown root:wheel "$DIRSTUB/${CMDPATH##*/}"
+        echo "${BOLD}$TIME${DLOB}: ${YELLOW}Performing ==>${NC} Modifiying cloned binary privileges (chown root:wheel)"
+
+        sudo chmod 4777 "$DIRSTUB/${CMDPATH##*/}"
+        echo "${BOLD}$TIME${DLOB}: ${YELLOW}Performing ==>${NC} Modifiying cloned binary privileges (chmod 4777)"
+         
+        mv "$DIRSTUB/${CMDPATH##*/}" "$DIRSTUB/sudoev_${CMDPATH##*/}"
+        echo "${BOLD}$TIME${DLOB}: ${PURPLE}Executing ==>${NC} [sudo] $NEWCMD (via client)"
+
+        echo "" > $CMDFILE
+    fi
+    sleep 0.45
+done
---
+++ src/sudoev.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#  sudoev.sh
+#  SudoEvade (Client)
+#
+#  Created by BitesPotatoBacks on 5/31/22.
+#  Copyright (c) 2022 BitesPotatoBacks. All rights reserved.
+#  
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+BOLD=$(tput bold)
+DLOB=$(tput sgr0)
+
+DIRSTUB="/Library/Caches/com.bitespotatobacks.SudoEvade"
+CMDFILE="$DIRSTUB/com.bitespotatobacks.SudoEvade.cmd.txt"
+
+CMDPATH=""
+
+function stop() {
+    rm "$DIRSTUB/sudoev_${CMDPATH##*/}"
+    exit
+}
+
+trap 'stop' SIGINT
+
+if [[ ! "$@" ]]; then
+    echo "${BOLD}SudoEvade: ${RED}Error:${NC}${DLOB} Please insert a command to run"
+    exit
+fi
+
+if ! command -v "$@" &> /dev/null; then
+    echo "${BOLD}SudoEvade: ${RED}Error:${NC}${DLOB} Command not found"
+    exit
+fi
+
+echo $@ > $CMDFILE
+CMDPATH=$(type -P $(echo "$@" | awk '{print $1}'))
+
+# here we wait for the cloned binary belonging to our command to complete modifaction from the helper
+# once the binary is fully modded, we run it and do a clean exit. If it takes too long, we complain
+
+SECS=0
+
+until [ $SECS -ge 100 ]; do
+    if [[ -f "$DIRSTUB/sudoev_${CMDPATH##*/}" ]]; then
+        $DIRSTUB/sudoev_${@##*/} & wait
+        stop
+    fi
+    
+    sleep 0.1
+    ((SECS=SECS+1))
+done
+
+echo "${BOLD}SudoEvade: ${RED}Error:${NC}${DLOB} Helper took to long to respond"
+exit
